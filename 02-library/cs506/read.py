4d2726acf04bf80266612475d4151a6ee7fd8848
Ji Zhao <zhaoji@bu.com>
2022-02-08 13:13:57 -0500

    this is lab3

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -1,6 +1,12 @@
+import pandas as pd
 def read_csv(csv_file_path):
     """
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+    csv_file_path = "/Users/zj/CS506 material/homework1-JiZhaoU23540628/train.csv"
+    df = pd.Dataframe(pd.read_cvs(csv-file_path))
+    return df
+
+        
+    
--- 02-library/cs506/sim.py
+++ 02-library/cs506/sim.py
@@ -1,3 +1,4 @@
+import numpy as np
 def euclidean_dist(x, y):
     res = 0
     for i in range(len(x)):
@@ -5,12 +6,22 @@ def euclidean_dist(x, y):
     return res**(1/2)
 
 def manhattan_dist(x, y):
-    raise NotImplementedError()
+    res = 0
+    for i in range(len(x)):
+        res += abs(x[i]-y[i])
+    return res
 
 def jaccard_dist(x, y):
-    raise NotImplementedError()
+    x =np.asarray(x)
+    y =np.asarray(y)
+    up = np.double(np.bitwise)_and((x!=y), np.bitwise_or(x != 0, y !=0)).sum())
+    down = np.double(np.bitwise_or(x != 0, y != 0).sum())
+    dis = (up/down)
+    return dis
+   
 
 def cosine_sim(x, y):
-    raise NotImplementedError()
+    dis = 1- np.dot(x,y)/np.linalg.norm(x)*np.linalg.norm(y))
+    return dis
 
 # Feel free to add more
