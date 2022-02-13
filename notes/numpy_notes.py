50a9b0a72a2b70a3ddb9ba1e3c7ec84b928cd151
Ji Zhao <zhaoji@bu.com>
2022-02-13 14:50:48 -0500

    add content to the file

--- notes/numpy_notes.py
+++ notes/numpy_notes.py
@@ -5,3 +5,9 @@ np.unravel_index(i , dis_array.shape)
 """
 np.unravel_index(i, dis_array.shape) is aming
 to return the original index of these elements in the originlal matrix (dis_array.shape)"""
+np.mat(A) # convert A to matrix
+np.zeros([2,3]) # form a 2*3 zero matrix
+np.dot(A,B) # dot multiple of A and B; 
+np.multiply(A,B) # elementwise multiply A by B
+A*B
+np.matmul(A,B)
